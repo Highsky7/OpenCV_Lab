0ea6fa48e1d02aad0993094e96bb27fd0102452e
Highsky7 <albert31115@gmail.com>
2025-03-22 22:49:26 +0900

    feat: Add files for my studying in March 22nd(lsm)

--- review_opencv/scripts/Basic_Image_Manipulations.py
+++ review_opencv/scripts/Basic_Image_Manipulations.py
@@ -5,7 +5,59 @@ import matplotlib.pyplot as plt
 
 from IPython.display import Image
 
-taiwan_img = cv2.imread("review_opencv/images/2.jpeg", 0)
-plt.imshow(taiwan_img, cmap="gray")
-print(taiwan_img)
+# taiwan_img = cv2.imread("review_opencv/images/2.jpeg", 0)
+# taiwan_img_small = cv2.resize(taiwan_img, (18, 18)) # resize 시 (W, H)순서
+# plt.imshow(taiwan_img_small, cmap="gray")
+# print(taiwan_img)
+# print(taiwan_img.shape)
+# plt.show()
+# Accessing Individual Pixels
+# print(taiwan_img_small[0, 0])
+# print(taiwan_img_small[0, 255])
+# Modifying Image Pixels
+# taiwan_img_small_copy = taiwan_img_small.copy()
+# taiwan_img_small_copy[0, 0] = 0
+# taiwan_img_small_copy[0, 1] = 0
+# taiwan_img_small_copy[1, 0] = 0
+# taiwan_img_small_copy[1, 1] = 0
+# plt.imshow(taiwan_img_small_copy, cmap="gray")
+# plt.show()
+# print(taiwan_img_small_copy)
+paris_img = cv2.imread("review_opencv/images/3.jpeg", cv2.IMREAD_COLOR)
+paris_img_rgb = cv2.cvtColor(paris_img, cv2.COLOR_BGR2RGB)
+# plt.imshow(paris_img_rgb)
+# plt.show()
+# cropped_paris_img = paris_img_rgb[750:1250, 500:1000] # H,W sequence(row, colomun)
+# plt.imshow(cropped_paris_img)
+# plt.show()
+# half_resized_cropped_paris_img = cv2.resize(cropped_paris_img, None, fx=0.5, fy=0.5)
+# plt.imshow(half_resized_cropped_paris_img)
+# plt.show()
+# desired_width = 100
+# desired_height = 100
+# dim = (desired_width, desired_height)
+# desired_resized_cropped_paris_img = cv2.resize(cropped_paris_img, dim, interpolation=cv2.INTER_AREA)
+# plt.imshow(desired_resized_cropped_paris_img)
+# plt.show()
+# desired_width = 100
+# aspect_ratio = desired_width / cropped_paris_img.shape[1]
+# desired_height = int(cropped_paris_img.shape[0] * aspect_ratio)
+# dim = (desired_width, desired_height)
+# ratio_cropped_paris_img = cv2.resize(cropped_paris_img, dsize=dim, interpolation=cv2.INTER_AREA)
+# plt.imshow(ratio_cropped_paris_img)
+# plt.show()
+# half_resized_cropped_paris_img = half_resized_cropped_paris_img[:, :, ::-1]
+# cv2.imwrite("review_opencv/images/3_half.jpeg", half_resized_cropped_paris_img)
+# Image(filename="review_opencv/images/3_half.jpeg")
+# cropped_paris_img = cropped_paris_img[:, :, ::-1]
+# cv2.imwrite("review_opencv/images/3_cropped.jpeg", cropped_paris_img)
+# Image(filename="review_opencv/images/3_cropped.jpeg")
+paris_img_rgb_horz = cv2.flip(paris_img_rgb, 1)
+paris_img_rgb_vert = cv2.flip(paris_img_rgb, 0)
+paris_img_rgb_both = cv2.flip(paris_img_rgb, -1)
+plt.figure(figsize=(20, 10)) # Image size is width: 20in x height: 10in
+plt.subplot(1, 4, 1), plt.imshow(paris_img_rgb_horz), plt.title("Horizontally Flipped")
+plt.subplot(1, 4, 2), plt.imshow(paris_img_rgb_vert), plt.title("Vertically Flipped")
+plt.subplot(1, 4, 3), plt.imshow(paris_img_rgb_both), plt.title("Both Flipped")
+plt.subplot(1, 4, 4), plt.imshow(paris_img_rgb), plt.title("Original Image")
 plt.show()
--- review_opencv/scripts/Image_Annotation.py
+++ review_opencv/scripts/Image_Annotation.py
@@ -1,3 +1,37 @@
 import os
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (20, 10)
+image = cv2.imread("review_opencv/images/4.jpeg", cv2.IMREAD_COLOR)
+plt.imshow(image[:, :, ::-1])
+plt.show()
+# print(image.shape)
+# better_image = image[:, 450:2048]
+# plt.imshow(better_image)
+# plt.show()
+# cv2.imwrite("review_opencv/images/4_better.jpeg", better_image)
+imageLine = image.copy()
+cv2.line(imageLine, (0, 0), (2047, 1535), (0, 255, 255), thickness=10, lineType=cv2.LINE_8) # x,y sequence
+plt.imshow(imageLine[:, :, ::-1])
+plt.show()
+image_rectangle = image.copy()
+cv2.rectangle(image_rectangle, (500, 1000), (1500, 500), (0, 255, 0), thickness=5, lineType=cv2.LINE_4)
+plt.imshow(image_rectangle[:, :, ::-1])
+plt.show()
+image_circle = image.copy()
+cv2.circle(image_circle, (1024, 768), 500, (255, 0, 0), thickness=10, lineType=cv2.LINE_AA)
+plt.imshow(image_circle[:, :, ::-1])
+plt.show()
+image_text = image.copy()
+text = "The Beauty of Paris above the Eiffel Tower"
+fontScale = 2.0
+fontFace = cv2.FONT_HERSHEY_COMPLEX
+fontColor = (255, 0, 0)
+fontThickness = 5
+
+cv2.putText(image_text, text, (100, 100), fontFace, fontScale, fontColor, fontThickness, lineType=cv2.LINE_AA)
+
+plt.imshow(image_text[:, :, ::-1])
+plt.show()
